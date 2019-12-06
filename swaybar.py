dd767fda84175439a32078297daa2f286dd1d1c5
Noam Bachmann <noam@helou.ch>
2019-12-06 22:16:31 +0100

    lot o' changes

--- swaybar.py
+++ swaybar.py
@@ -1,30 +1,57 @@
 #!/usr/bin/env python3
 
+# from os import system
 from datetime import datetime
-from psutil import disk_usage, sensors_battery
-from psutil._common import bytes2human
-from socket import gethostname, gethostbyname
+from psutil import sensors_battery
+# from socket import gethostname, gethostbyname
 from subprocess import check_output
 from sys import stdout
 from time import sleep
+from math import floor
 
 def write(data):
-    stdout.write('%s\n' % data)
+    stdout.write(data + '\n')
     stdout.flush()
 
+def nice_bat(percent, charging):
+    eight = int(round(percent * 8 / 100))
+    mapping = ['░','▁','▂','▃','▄','▅','▆','▇','█']
+    if eight <= 2:
+        return '<span foreground="red"  background="#efefef">' + mapping[eight] + '</span>'
+    if eight <= 8 and eight >= 0:
+        return '<span foreground="green" background="#efefef">' + mapping[eight] + '</span>'
+    return '╳'
+    # This ╳ is a box drawing charcters with full width
+
 def refresh():
-    disk = bytes2human(disk_usage('/').free)
-    ip = gethostbyname(gethostname())
+    # light = system("light -G")
+    # ip = gethostbyname(gethostname())
     try:
         ssid = check_output("iwgetid -r", shell=True).strip().decode("utf-8")
-        ssid = "(%s)" % ssid
+        ssid = 'Connected: ' + ssid if ssid else 'No Internet'
     except Exception:
-        ssid = "None"
-    battery = int(sensors_battery().percent)
-    status = "Charging" if sensors_battery().power_plugged else "Discharging"
-    date = datetime.now().strftime('%h %d %A %H:%M')
-    format = "Space: %s | Internet: %s %s | Battery: %s%% %s | Date: %s"
-    write(format % (disk, ip, ssid, battery, status, date))
+        ssid = 'No Internet'
+    ssid = ssid.ljust(20)
+    bat_sens = sensors_battery()
+    if bat_sens:
+        bat_percent = int(bat_sens.percent)
+        bat_symbol = nice_bat(bat_percent, bat_sens.power_plugged)
+        bat_status = ("ϟ" if bat_sens.power_plugged else ' ')
+        if bat_sens.secsleft and bat_sens.secsleft >= 0:
+            bat_time = (str(floor(bat_sens.secsleft / 3600)).rjust(2)
+                    + ':'
+                    + str(floor(bat_sens.secsleft % 3600 / 300) * 5).rjust(2, '0') + ' left')
+        elif bat_sens.power_plugged:
+            bat_time = 'Charging  '
+        else:
+            bat_time = ' ' * 10
+    else:
+        bat_percent = '--'
+        bat_symbol = '╳'
+        bat_status = ' '
+        bat_time = ' ' * 10
+    date = datetime.now().strftime('%Y-%m-%d %a %H:%M:%S')
+    write(f"│ {ssid} │ {bat_status}{bat_symbol} {str(bat_percent).rjust(2, '0')}% {bat_time} │ {date}")
 
 while True:
     refresh()
